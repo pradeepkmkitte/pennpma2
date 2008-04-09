d1c984158a60d04411b2f236c38c1125cdd436d6
scottwkyle <scottwkyle@e148c0d5-d148-0410-8e6a-8573859296cf>
2008-04-09 05:28:03 +0000

    changes

--- src/record.java
+++ src/record.java
@@ -4,71 +4,70 @@ import java.util.*;
 
 public class record{
 
-  static AudioFormat format = new AudioFormat(8000.0F, 16, 1, true, true);
-  static TargetDataLine line;
-  static Scanner sc = new Scanner(System.in);
-  
-  
-  String fileName;
-//  String samplesFolder="speaker/training-samples";
-  String samplesFolder="training-samples";  
-     
-  record(){
-      
-	  fileName = "temp.wav";
-      
-	  try{
-	//    Recording starts by starting a 'capture thread', 
-	//    once the thread is opened, it returns
-	      DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-	      line = (TargetDataLine) AudioSystem.getLine(info);
-	  }
-	  catch (Exception e){
-		  System.out.println("error creating record");
-	  }
-  
-            new SaveThread().start();
-
-            System.out.print("\tpress 'Enter' to stop recording");
-            String end = sc.nextLine();
-            
-            stopRecord();
-  }
-  
-  public void stopRecord(){
-	  line.stop();
-	  line.close();
-  }
-  
-  public String getName(){
-          return fileName;
-  }
+	static AudioFormat format = new AudioFormat(8000.0F, 16, 1, true, true);
+	static TargetDataLine line;
+	static Scanner sc = new Scanner(System.in);
 
 
-/*
- * This is an inner Thread class that saves the input into a designated file
- */
+	String fileName;
+	String samplesFolder="speaker/training-samples";
 
-        class SaveThread extends Thread{
-          public void run(){
-        
-//              set the file destination
-            File newSample = new File(samplesFolder+"/" + fileName);
-        
-            try{
-//            open and start a data line to collect data with the designated format
-              line.open(format);
-              line.start();
-              
-//            write to the file using the line input with correct type and destination
-              AudioSystem.write( new AudioInputStream(line), AudioFileFormat.Type.WAVE, newSample);
-            }
-//          Exceptions thrown by AudioSystem.write and line.open
-            catch (Exception e){
-              e.printStackTrace();
-            }
-          }
-          
-        }
+	record(){
+
+		fileName = "temp.wav";
+
+		try{
+			//    Recording starts by starting a 'capture thread', 
+			//    once the thread is opened, it returns
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+			line = (TargetDataLine) AudioSystem.getLine(info);
+		}
+		catch (Exception e){
+			System.out.println("error creating record");
+		}
+
+		new SaveThread().start();
+
+		System.out.print("\tpress 'Enter' to stop recording");
+		String end = sc.nextLine();
+
+		stopRecord();
+	}
+
+	public void stopRecord(){
+		line.stop();
+		line.close();
+	}
+
+	public String getName(){
+		return fileName;
+	}
+
+
+	/*
+	 * This is an inner Thread class that saves the input into a designated file
+	 */
+
+	class SaveThread extends Thread{
+		public void run(){
+
+//			set the file destination
+			File newSample = new File(samplesFolder+"/" + fileName);
+
+			try{
+//				open and start a data line to collect data with the designated format
+				line.open(format);
+				line.start();
+
+//				write to the file using the line input with correct type and destination
+				AudioSystem.write( new AudioInputStream(line), AudioFileFormat.Type.WAVE, newSample);
+			}
+//			Exceptions thrown by AudioSystem.write and line.open
+			catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
